965d954383b435d73578fa0d1cc1a0856f9c2717
maxzhangcong <maxzhangcong@163.com>
2018-03-02 16:49:46 +0800

    0302

---
+++ search/binary_search.py
@@ -0,0 +1,55 @@
+# _*_ coding: utf-8 _*_
+# @Time     :2018/3/2 14:59
+# @Author   :maxzhangcong
+# @Email    :maxzhangcong@163.com
+
+"""
+    *************模块文档注释**************
+
+    使用二分查找，要求是查找的序列首先应该是排序之后的序列,并支持下标索引
+    二分查找只能作用到有序的list上
+
+
+"""
+
+
+def binary_search(arr, item):
+    """二分查找,递归方式简便
+        复杂度logn
+    """
+    n = len(arr)
+    if n > 0:
+        mid_point = n // 2
+        if arr[mid_point] == item:
+            return True
+        elif item < arr[mid_point]:
+            return binary_search(arr[:mid_point], item)
+        else:
+            return binary_search(arr[mid_point + 1:], item)
+    return False
+
+
+def binary_search_2(arr, item):
+    """二分查找非递归版本
+        主要起始位置的下标来确定中间元素
+        复杂度
+    """
+    n = len(arr)
+    first_point = 0
+    last_point = n - 1
+    while first_point <= last_point:
+        mid_point = (first_point + last_point) // 2
+        if arr[mid_point] == item:
+            return True
+        elif item < arr[mid_point]:
+            last_point = mid_point - 1
+        else:
+            first_point = mid_point+1
+    return False
+
+
+if __name__ == '__main__':
+    a = [0, 0, 3, 6, 9, 12, 21, 45, 123, 454, 3123, 4645, 43078, 2346767, 23432547, 123123123, 657463453]
+    print(binary_search(a, 43078))
+    print('*'*23)
+    print(binary_search_2(a, 43078))
--- tree/code_make_tree.py
+++ tree/code_make_tree.py
@@ -64,25 +64,52 @@ class Tree(object):
 
         while queue:
             cur_node = queue.pop(0)
-            print(cur_node.item)  # 打印元素
+            print(cur_node.item, end='')  # 打印元素
             if cur_node.lchild is not None:
                 queue.append(cur_node.lchild)
             if cur_node.rchild is not None:
                 queue.append(cur_node.rchild)
 
     """深度遍历有三种方式存在
-    三种遍历方式没有从right开始的方式，遍历方法树及子树的嵌套，依次按照顺序进行
+    三种遍历方式没有从right开始的方式，遍历方法树及子树的嵌套，依次按照顺序进行、按照规则撞到南墙再回头
     1、从root开始遍历root、left、right 先序遍历
     2、从left开始遍历left、root、right 中序遍历
     3、从left开始遍历left、right、root 后序遍历
+    
+    无论是什么序遍历，都是去打印相对的根节点,不要想太多简化问题，只不过是先处理左半部分还是右半部分的子树的问题
+    
     """
 
+    def preorder(self, root_node):  # 先序遍历,只要代码涉及到递归的话都是异常简单的，先序循环遍历都是在转换跟，需要把root传进去
+
+        if root_node is None:  # 不是根或子根节点的话直接退出
+            return
+        print(root_node.item, end='')
+        self.preorder(root_node.lchild)
+        self.preorder(root_node.rchild)
+
+    def inorder(self, root_node):  # 中序遍历
+        if root_node is None:
+            return
+        self.inorder(root_node.lchild)
+        print(root_node.item, end='')
+        self.inorder(root_node.rchild)
+
+    def postorder(self, root_node):  # 后序遍历
+        if root_node is None:
+            return
+        self.postorder(root_node.lchild)
+        self.postorder(root_node.rchild)
+        print(root_node.item, end='')
 
 
-
+"""给出两种遍历的结果把树画出来，但是必须包含中序的遍历结果，才能把树画出来，只有根节点才能把左右子树分开确定
+    依次逻辑推断出根，分出左右进行判断。
+"""
 
 if __name__ == "__main__":
     tree = Tree()
+    tree.add(0)
     tree.add(1)
     tree.add(2)
     tree.add(3)
@@ -92,4 +119,14 @@ if __name__ == "__main__":
     tree.add(7)
     tree.add(8)
     tree.add(9)
+    print('广度遍历')
     tree.breadth_travel()
+    print(' ')
+    print('深度先序遍历')
+    tree.preorder(tree.root)
+    print(' ')
+    print('深度中序遍历')
+    tree.inorder(tree.root)
+    print(' ')
+    print('深度后序遍历')
+    tree.postorder(tree.root)
